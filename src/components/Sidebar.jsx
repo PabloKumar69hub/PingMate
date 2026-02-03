627fb168e7f8aa76f19df440744a3356599f0e32
Gitesh <sargunamgitesh@gmail.com>
2026-02-03 22:30:09 +0530

    Fix setSelectedUser typo in Sidebar click handler

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,7 +28,7 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
       </div>
       <div className='flex flex-col'>
         {userDummyData.map((user,index)=>(
-          <div onClick={()=> {setselectedUser(user)}}
+          <div onClick={()=> {setSelectedUser(user)}}
            key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded 
           cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[282142]/50'}`}>
             <img src={user?.profilePic || assets.avatar_icon} alt="" 
