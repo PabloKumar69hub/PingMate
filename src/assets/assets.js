9db44077518ddaf14410aae030c70ec19fdde48a
GITESH-S <230514.cs@rmkec.ac.in>
2026-02-06 19:08:05 +0530

    Rebrand app to PingMate and updated login UI

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,10 +4,10 @@ import help_icon from './help_icon.png'
 import logo_icon from './logo_icon.svg'
 import logo_big from './logo_big.svg'
 import logo from './logo.png'
-import profile_richard from './profile_richard.png'
-import profile_alison from './profile_alison.png'
-import profile_enrique from './profile_enrique.png'
-import profile_marco from './profile_marco.png'
+import profile_kishore from './profile_kishore niggesh.jpg'
+import profile_harish from './profile_harish.png'
+import profile_suraj from './profile_suraj.png'
+import profile_harsha from './profile_harsha.jpeg'
 import profile_Ramgoat from './profile_Ramgoat.jpeg'
 import search_icon from './search_icon.png'
 import send_button from './send_button.svg'
@@ -15,12 +15,12 @@ import menu_icon from './menu_icon.png'
 import arrow_icon from './arrow_icon.png'
 import code from './code.svg'
 import bgImage from './bgImage.svg'
-import pic1 from './pic1.png'
-import pic2 from './pic2.png'
-import pic3 from './pic3.png'
-import pic4 from './pic4.png'
+import pic1 from './pic1.jpeg'
+import pic2 from './pic2.jpeg'
+import pic3 from './pic3.jpeg'
+import pic4 from './pic4.jpeg'
 import img1 from './img1.jpg'
-import img2 from './img2.jpg'
+import img2 from './img2.png'
 
 const assets = {
     avatar_icon,
@@ -40,15 +40,15 @@ const assets = {
 
 export default assets;
 
-export const imagesDummyData = [pic1, pic2, pic3, pic4, pic1, pic2]
+export const imagesDummyData = [pic1, pic2, pic3, pic4,]
 
 export const userDummyData = [
     {
         "_id": "680f50aaf10f3cd28382ecf2",
         "email": "test1@greatstack.dev",
-        "fullName": "Alison Martin",
-        "profilePic": profile_alison,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "fullName": "Harish",
+        "profilePic": profile_harish,
+        "bio": "Manmadha kunj",
     },
     {
         "_id": "680f50e4f10f3cd28382ecf9",
@@ -60,23 +60,23 @@ export const userDummyData = [
     {
         "_id": "680f510af10f3cd28382ed01",
         "email": "test3@greatstack.dev",
-        "fullName": "Enrique Martinez",
-        "profilePic": profile_enrique,
+        "fullName": "Suraj",
+        "profilePic": profile_suraj,
         "bio": "Hi Everyone, I am Using QuickChat",
     },
     {
         "_id": "680f5137f10f3cd28382ed10",
         "email": "test4@greatstack.dev",
-        "fullName": "Marco Jones",
-        "profilePic": profile_marco,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "fullName": "Harsha Mama",
+        "profilePic": profile_harsha,
+        "bio": "Hi there :)",
     },
     {
         "_id": "680f516cf10f3cd28382ed11",
         "email": "test5@greatstack.dev",
-        "fullName": "Richard Smith",
-        "profilePic": profile_richard,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "fullName": "kishore niggesh",
+        "profilePic": profile_kishore,
+        "bio": "i like kanye zest",
     }
 ]
 
@@ -85,7 +85,7 @@ export const messagesDummyData = [
         "_id": "680f571ff10f3cd28382f094",
         "senderId": "680f5116f10f3cd28382ed02",
         "receiverId": "680f50e4f10f3cd28382ecf9",
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "text": "sweeney is such a baddie!!!!!!",
         "seen": true,
         "createdAt": "2025-04-28T10:23:27.844Z",
     },
@@ -93,7 +93,7 @@ export const messagesDummyData = [
         "_id": "680f5726f10f3cd28382f0b1",
         "senderId": "680f50e4f10f3cd28382ecf9",
         "receiverId": "680f5116f10f3cd28382ed02",
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "text": "nah nibba rukmini better fr",
         "seen": true,
         "createdAt": "2025-04-28T10:23:34.520Z",
     },
@@ -101,7 +101,7 @@ export const messagesDummyData = [
         "_id": "680f5729f10f3cd28382f0b6",
         "senderId": "680f5116f10f3cd28382ed02",
         "receiverId": "680f50e4f10f3cd28382ecf9",
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "text": "sybau ffs sweeney so goated  in euphoria :D",
         "seen": true,
         "createdAt": "2025-04-28T10:23:37.301Z",
     },
@@ -109,7 +109,7 @@ export const messagesDummyData = [
         "_id": "680f572cf10f3cd28382f0bb",
         "senderId": "680f50e4f10f3cd28382ecf9",
         "receiverId": "680f5116f10f3cd28382ed02",
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "text": "nah nibba i prefer rukmini :)",
         "seen": true,
         "createdAt": "2025-04-28T10:23:40.334Z",
     },
@@ -133,7 +133,7 @@ export const messagesDummyData = [
         "_id": "680f5748f10f3cd28382f0ca",
         "senderId": "680f5116f10f3cd28382ed02",
         "receiverId": "680f50e4f10f3cd28382ecf9",
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "text": "sweeney wins this battle !!!!!!! sydney sweeney>>>rukmini.",
         "seen": true,
         "createdAt": "2025-04-28T10:24:08.523Z",
     }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
+import React, { use, useState } from 'react'
+import assets from '../assets/assets'
 
 const LoginPage = () => {
+  
+      const [currstate,setCurrstate]=useState("Sign Up")
+      const [fullname,setFullname]=useState("")
+      const [email,setEmail]=useState("")
+      const [password,setPassword]=useState("")
+      const [bio,setBio]=useState("")
+      const [isDataSubmitted,setIsDataSubmitted]=useState(false);
+
+  
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+   
+   <div className='min-h-screen bg-[#8185B2]/10 backdrop-blur-lg flex items-center 
+ justify-center gap-8 sm:justify-evenly max-sm:flex-col'>
+
+      
+      {/*------------left area-----------------*/}  
+<div className="flex flex-col items-center gap-4">
+  <img src={assets.logo_icon} alt="PingMate logo" className='w-30 sm:w-37' />
+  <h1 className="text-white text-3xl font-medium tracking-wide">
+    PingMate
+  </h1>
+</div>
+      {/*------------right area----------------*/}
+      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex
+      flex-col gap-6 rounded-lg shadow-lg'>
+        <h2 className="flex items-center gap-2 text-xl font-medium">
+  {currstate}
+  <img src={assets.arrow_icon} alt="arrow" className='w-5 cursor-pointer opacity-80' />
+  </h2>
+  {currstate === "Sign up" && !isDataSubmitted &&  (
+    <input onChange={(e)=>setFullname(e.target.value)} value={fullname}
+    type="text" className='p-2 border border-gray-500 rounded-md 
+    focus:outline-none' placeholder="Full Name" required/>
+  )}
+    {!isDataSubmitted && (
+      <>
+      <input onChange={(e)=>setEmail(e.target.value)} value={email} 
+      type="email" placeholder='Email Address' required className='p-2 
+      border border-gray-500 rounded-md focus:outline-none focus:ring-2 
+      focus:ring-indigo-500' />
+      <input onChange={(e)=>setPassword(e.target.value)} value={password} 
+      type="password" placeholder='Password' required className='p-2 
+      border border-gray-500 rounded-md focus:outline-none focus:ring-2 
+      focus:ring-indigo-500' />
+
+      </>
+    )}
+    {
+      currstate === "Sign up" && isDataSubmitted && (
+        <textarea onChange={()=>setBio(e.target.value)} value={bio} 
+        rows={4} className='p-2 border border-gray-500 rounded-md 
+        focus:outline-none focus:ring-2 focus:ring-indigo-500'>“How should others see you?”</textarea>
+      )
+    }
+    <button>
+      {currstate === "Sign up" ? "Create Account" : "Login Now"}
+      <div className='flex items-center gap-2 text-sm text-gray-500'>
+        <input type="checkbox" />
+        <p>Agree to the terms of use & privacy policy.</p>
+      </div>
+    </button>
+  </form>
+  </div>
   )
 }
 
