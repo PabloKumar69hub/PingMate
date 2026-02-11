cb4922a631a3f555aea622c6b934197d735add85
GITESH-S <230514.cs@rmkec.ac.in>
2026-02-11 13:22:12 +0530

    server is updated for vercel

--- server/server.js
+++ server/server.js
@@ -63,8 +63,12 @@ app.use("/api/messages", messageRouter);
 
 // Connect MongoDB
 await connectDB();
-
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
   console.log("Server is running on PORT:", PORT)
 );
+}
+
+// Export sever for Vercel
+export default server;
